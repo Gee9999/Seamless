cfd3d951f476549c7156d8f10c9ae1503b21c705
Gee9999 <geozi1@gmail.com>
2025-05-15 10:28:52 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -3,36 +3,30 @@ from PIL import Image
 import numpy as np
 import io
 
-def make_seamless_tile(img: Image.Image, blend_width=20) -> Image.Image:
+def create_mirrored_tile(img: Image.Image) -> Image.Image:
     img = img.convert("RGB")
-    arr = np.array(img)
-    h, w, _ = arr.shape
+    w, h = img.size
 
-    # Offset image by 50% in both directions
-    offset_arr = np.roll(arr, shift=(h // 2, w // 2), axis=(0, 1))
+    # Create 2x2 grid with mirrored edges
+    left_top = img
+    right_top = img.transpose(Image.FLIP_LEFT_RIGHT)
+    left_bottom = img.transpose(Image.FLIP_TOP_BOTTOM)
+    right_bottom = img.transpose(Image.ROTATE_180)
 
-    # Blend vertical seam
-    for i in range(-blend_width, blend_width):
-        alpha = 0.5 + 0.5 * (i / blend_width)
-        offset_arr[:, (w // 2) + i, :] = (
-            alpha * offset_arr[:, (w // 2) + i, :] +
-            (1 - alpha) * offset_arr[:, (w // 2) + i - 1, :]
-        ).astype(np.uint8)
+    new_img = Image.new("RGB", (w * 2, h * 2))
+    new_img.paste(left_top, (0, 0))
+    new_img.paste(right_top, (w, 0))
+    new_img.paste(left_bottom, (0, h))
+    new_img.paste(right_bottom, (w, h))
 
-    # Blend horizontal seam
-    for i in range(-blend_width, blend_width):
-        alpha = 0.5 + 0.5 * (i / blend_width)
-        offset_arr[(h // 2) + i, :, :] = (
-            alpha * offset_arr[(h // 2) + i, :, :] +
-            (1 - alpha) * offset_arr[(h // 2) + i - 1, :, :]
-        ).astype(np.uint8)
-
-    return Image.fromarray(offset_arr)
+    # Crop to center
+    center_crop = new_img.crop((w // 2, h // 2, w + w // 2, h + h // 2))
+    return center_crop
 
 # Streamlit UI
-st.set_page_config(page_title="Seamless Tile Generator", layout="centered")
-st.title("🧩 Seamless Tile Generator")
-st.markdown("Upload an image and generate a seamless tile pattern.")
+st.set_page_config(page_title="Mirrored Seamless Tile Generator", layout="centered")
+st.title("🪞 Mirrored Seamless Tile Generator")
+st.markdown("Upload an image to generate a truly seamless tile using edge mirroring.")
 
 uploaded_file = st.file_uploader("Choose an image", type=["png", "jpg", "jpeg"])
 
@@ -41,10 +35,10 @@ if uploaded_file:
     st.image(image, caption="Original Image", use_column_width=True)
 
     with st.spinner("Creating seamless tile..."):
-        seamless_tile = make_seamless_tile(image)
+        seamless_tile = create_mirrored_tile(image)
 
     st.success("Seamless tile created!")
-    st.image(seamless_tile, caption="Seamless Tile Preview", use_column_width=True)
+    st.image(seamless_tile, caption="Mirrored Seamless Tile Preview", use_column_width=True)
 
     buf = io.BytesIO()
     seamless_tile.save(buf, format="PNG")
@@ -53,6 +47,6 @@ if uploaded_file:
     st.download_button(
         label="📥 Download Seamless Tile",
         data=byte_im,
-        file_name="seamless_tile.png",
+        file_name="mirrored_seamless_tile.png",
         mime="image/png"
     )
